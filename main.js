52f021e58027c8afea452b1190e7c6bf120ab043
Tarcísio <tarcisiojunior@gmail.com>
2016-03-11 00:36:10 -0300

    Elaborando ideia...

--- main.js
+++ main.js
@@ -5,7 +5,22 @@
 var config = {
     host: 'localhost',
     port: 3306,
-    db: 'test',
+    database: 'test',
     user: 'root',
     passwrod: 'root'
 };
+
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection(config);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
